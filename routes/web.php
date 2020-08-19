944f39688c0e5fa5a01cae9dfe1985173d325e87
boa0421 <boa41109@gmail.com>
2020-08-19 11:12:19 +0000

    create post view file

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,12 @@
 
 Route::get('/', 'PostsController@index');
 Route::get('post/show', 'PostsController@show');
+
+Route::group(['prefix' => 'admin'], function() {
+     Route::get('post/create', 'Admin\PostsController@add');
+     Route::post('news/create', 'Admin\PostsController@create');
+     Route::get('post/index', 'Admin\PostsController@index');
+     Route::get('post/edit', 'Admin\PostsController@edit');
+     Route::post('post/edit', 'Admin\PostsController@update');
+     Route::get('post/delete', 'Admin\PostsController@delete');
+});
