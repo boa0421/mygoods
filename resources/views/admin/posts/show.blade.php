2586186c047a0cdb5ca5d5531ff783fed60fb8ab
boa0421 <boa41109@gmail.com>
2020-08-25 12:35:02 +0000

    create posts.scss

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.admin')
-@section('title', '登録済みPostの一覧')
+@extends('layouts.posts')
+@section('title', '登録済みPostの詳細')
 
 @section('content')
     <div class="container">
@@ -7,42 +7,52 @@
             <h2>Post詳細</h2>
         </div>
         <div class="row">
-            <div class="col-md-4">
-                <!--<a href="{{ action('Admin\PostsController@add') }}" role="button" class="btn btn-primary">新規作成</a>-->
+            <div class="main">
+                <section class="card-main">
+                    <div class="image">
+                        <img class="card-img" src=="{{ asset('storage/image/' . $post->image) }}" alt="post 画像">
+                    </div>
+                    <div class="card-content">
+                        <!--<h1 class="card-title">画像</h1>-->
+                    </div>
+                    <div class="card-link">
+                        <div>
+                            <h3>いいねボタン</h3>
+                        </div>
+                        <div>
+                            <h3>フォローボタン</h3>
+                        </div>
+                        <div>
+                            <h3>コメント蘭</h3>
+                        </div>
+                    </div>
+                </section>
             </div>
-        </div>
-        <div class="row">
-            <div class="list-news col-md-12 mx-auto">
-                <div class="row">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th width="10%">ID</th>
-                                <th width="20%">タイトル</th>
-                                <th width="30%">本文</th>
-                                <th width="20%">アイテム</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>{{ $post->id }}</th>
-                                <td>{{ \Str::limit($post->title, 100) }}</td>
-                                <td>{{ \Str::limit($post->content, 250) }}</td>
-                                <td>{{ \Str::limit($post->item_name, 100) }}</td>
-                                <td>
-                                    <div>
-                                        <a href="{{ action('Admin\PostsController@edit', ['id' => $post->id]) }}">編集</a>
-                                    </div>
-                                    <div>
-                                        <a href="{{ action('Admin\PostsController@delete', ['id' => $post->id]) }}">削除</a>
-                                    </div>
-                                    <div>
-                                        <a href="{{ action('Admin\ItemsController@add', ['post_id' => $post->id]) }}">アイテム追加</a>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+            
+            <div id="sidebar">
+                <div class="banner">
+                    <section class="card-side">
+                        <div class="card-content">
+                            <h1 class="card-title">{{ \Str::limit($post->title, 100) }}</h1>
+                            <p class="card-text">{{ \Str::limit($post->content, 250) }}</p>
+                            @foreach($items as $post)
+                                <div class="items">
+                                    <p class="card-text">{{ $post->item_name }}</p>
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="card-link">
+                            <div>
+                                <a href="{{ action('Admin\PostsController@edit', ['id' => $post->id]) }}">編集</a>
+                            </div>
+                            <div>
+                                <a href="{{ action('Admin\PostsController@delete', ['id' => $post->id]) }}">削除</a>
+                            </div>
+                            <div>
+                                <a href="{{ action('Admin\ItemsController@add', ['post_id' => $post->id]) }}">アイテム追加</a>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
