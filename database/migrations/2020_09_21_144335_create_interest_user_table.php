956f5c84b17eb66debab0b22c1de1e43e1ad3a95
boa0421 <boa41109@gmail.com>
2020-09-21 14:51:42 +0000

    add intertests_table intertest_user_table

---
+++ database/migrations/2020_09_21_144335_create_interest_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInterestUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interest_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('interest_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+            
+            $table->foreign('interest_id')->references('id')->on('interests')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interest_user');
+    }
+}
